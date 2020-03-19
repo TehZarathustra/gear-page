cf9adf64b3e71e96509f2ca90ec28e1cbab33cc8
v-prusakov <v-prusakov@yandex-team.ru>
2020-03-19 13:53:23 +0300

    Adds routes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 const getWishlistData = require('./wishlist');
+const getRaidLog = require('./raidLog');
 
 app.use(express.static(path.join(__dirname, '../build')));
 
@@ -11,8 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
+app.get('/kek', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 app.get('/data', function (req, res) {
 	return getWishlistData(req, res);
+});
+
+app.get('/raid-log', function (req, res) {
+	return getRaidLog(req, res);
+});
+
+app.get('/raid-log/:player', function (req, res) {
+	return getRaidLog(req, res);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
