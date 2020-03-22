55c8ec8026009e881397776fb7a4f83dd86d5f6c
v-prusakov <v-prusakov@yandex-team.ru>
2020-03-22 23:38:42 +0300

    Adds random to Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './styles.css';
 
 const Loader = () => {
+	const chance = Math.random() < 0.5;
+
 	return (
 		<div class="loader">
-			<div class="loader__item loader_sparkles"></div>
-			<div class="loader__item loader_rainbows"></div>
+			<div class={`loader__item ${chance ? 'loader_sparkles' : 'loader_sparkles_shuffled'}`} />
+			<div class={`loader__item ${chance ? 'loader_rainbows' : 'loader_rainbows_shuffled'}`} />
 		</div>
 	);
 }
